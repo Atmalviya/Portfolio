680b692ab7bbf31fc839a58b4c66309c509fa1c7
Atul <atulmalviyav2@gmail.com>
2024-07-27 14:33:42 +0530

    constant data

---
+++ src/constants/index.js
@@ -0,0 +1,83 @@
+import project1 from "../assets/projects/chatDashboard.png";
+import project2 from "../assets/projects/notesAppDashboard.png";
+import project3 from "../assets/projects/AuthApp.jpg";
+
+export const HERO_CONTENT = `I am a passionate full stack developer with a knack for crafting robust and scalable web applications. With 1 years of hands-on experience, I have honed my skills in front-end technologies like React and Next.js, as well as back-end technologies like Node.js, MySQL, PostgreSQL, and MongoDB. My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.`;
+
+export const ABOUT_TEXT = `I am a dedicated and versatile full stack developer with a passion for creating efficient and user-friendly web applications. With 1 years of professional experience, I have worked with a variety of technologies, including React, Next.js, Node.js, MySQL, PostgreSQL, and MongoDB. My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, exploring new technologies, and contributing to open-source projects.`;
+
+export const EXPERIENCES = [
+  {
+    year: "July 2023 - Present",
+    role: " Software Developer Engineer",
+    company: "Open Sense Labs",
+    description: `Spearheaded front-end development efforts within a collaborative team environment, and Implemented backend logic to optimize data processing, resulting in a notable 25% reduction in response times, enhancing overall system
+efficiency.
+• Leveraged Node.js skills to enhance backend functionalities and streamline data processing tasks, contributing to improved system performance.
+`,
+    technologies: ["Javascript", "React.js", "Next.js", "Node.js", "mongoDB"],
+  },
+  {
+    year: "Sep 2022 - Feb 2023",
+    role: "Frontend Developer",
+    company: "Learn Tech Pvt. Ltd.",
+    description: `Developed user-friendly and responsive designs as a Front-End Developer intern, ensuring seamless user experiences across various devices and
+screen sizes.
+• Implemented optimizations in projects aimed at reducing site loading time, resulting in improved website performance and enhanced user
+satisfaction.`,
+    technologies: ["HTML", "CSS", "JavaScript", "React.js", "mySQL"],
+  },
+];
+
+export const EDUCATION = [
+  {
+    year: "July 2019 - 2023",
+    role: " Computer Science and Engineering",
+    company: "Jabalpur Engineering College, Jabalpur",
+    description: `Led a team in developing and maintaining web applications using JavaScript, React.js, and Node.js. Implemented RESTful APIs and integrated with MongoDB databases. Collaborated with stakeholders to define project requirements and timelines.`,
+    technologies: ["Javascript", "React.js", "Next.js", "Node.js", "mongoDB"],
+  },
+  {
+    year: "Jan 2022 ",
+    role: "CS50x Introduction to Computer Science",
+    company: "Harvard University",
+    description: `An intensive introduction to the intellectual enterprises of computer science and the art of programming. Topics included abstraction, algorithms, data structures, encapsulation, resource management, security, software engineering, and web development. The course included problem sets inspired by real-world domains of cryptography, biology, finance, forensics, and gaming.`,
+    technologies: ["C++", "Python", "JavaScript", "CyberSecurity", "SQL", "Web Development"],
+  },
+];
+
+export const PROJECTS = [
+  {
+    title: "Real Time Chat Application",
+    image: project1,
+    link:"https://github.com/Atmalviya/Chat-App",
+    hostLink:"https://notes-app-fe.up.railway.app/",
+    description:
+      "The real-time chat application built with Node.js employs WebSockets for instant messaging, ensuring low-latency communication. It features robust user authentication and authorization mechanisms for secure access.",
+    technologies: ["React", "Node.js", "Express", "MongoDB", "WebSockets"],
+  },
+  {
+    title: "Notes Application",
+    image: project2,
+    link:"https://github.com/Atmalviya/Notes-App-Fullstack",
+    hostLink:"https://notes-app-fe.up.railway.app/",
+    description:
+      "This is a full-stack notes application built using the MERN stack. The application allows users to login to their account and create, read, update, and delete notes.",
+    technologies: ["React", "Node.js", "Express", "MongoDB"],
+  },
+  {
+    title: "Authentication App",
+    image: project3,
+    link:"https://github.com/Atmalviya/Authentication-App",
+    hostLink:"https://notes-app-fe.up.railway.app/",
+    description:
+      "This is an authentication application where users can register themselves, log in, view and update their profiles, and reset their passwords using an OTP sent to their email.",
+    technologies: ["React", "Tailwin CSS", "Node.js", "Express", "MongoDB", "JWT"],
+  }
+];
+
+export const CONTACT = {
+  address: "Bhopal Madhya Pradesh, India",
+  phoneNo: "+91 7987579681",
+  email: "atulmalviyawork@gmail.com",
+};
